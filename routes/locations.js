f30a5e8a75ea0796a6cc2c42cf3138503ce1a46b
Gregory D. Hopkins <greg.hopkins@gmail.com>
2015-11-21 16:32:37 -0500

    Get all locations

--- routes/locations.js
+++ routes/locations.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var router = express.Router();
 
-/* GET home page. */
+require('../models/location.model');
+var Location = mongoose.model('Location');
+
 router.get('/', function(req, res, next) {
-  res.status(200).json({locations: []});
+  Location.find({}, function (error, locations) {
+    if (error) {
+      res.status(500).json({
+        message: 'Could not find any locations',
+        error: error
+      })
+    } else {
+      res.status(200).json(locations);
+    }
+  })
 });
 
 module.exports = router;
