c45179b3d3cefacd98e48f52d71f3839c20f6d18
Sarah Gay <sarah@riversagile.com>
2015-11-21 16:51:42 -0500

    added search by brewery and beer

--- routes/beers.js
+++ routes/beers.js
@@ -16,9 +16,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/typeahead', function (req, res, next) {
-  var regex = RegExp(req.query.name, 'i');
+  var brewery = req.query.brewery;
+  var beer = req.query.beer;
+  var name = req.query.name;
 
-  return Beer.find({$or:[ {'name':{ $regex: regex }}, {'brewery':{ $regex: regex }}]}, function (error, beers) {
+  var regex = RegExp(name, 'i');
+
+  var search = {$or:[ {'name': { $regex: regex }}, {'brewery': { $regex: regex }}]};
+
+  if (brewery) {
+    regex = RegExp(brewery, 'i');
+    search = {brewery: { $regex: regex }};
+  }
+
+  if (beer) {
+    regex = RegExp(beer, 'i');
+    search = {name: { $regex: regex }};
+  }
+
+  return Beer.find(search, function (error, beers) {
     if (error) {
       return console.error("Could not query beers.", error);
     } else {
