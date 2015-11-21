8a3445770337cb7f5d0c060ad015bf8cdcc4099f
Sarah Gay <sarah@riversagile.com>
2015-11-21 16:23:56 -0500

    updated beers

--- routes/beers.js
+++ routes/beers.js
@@ -6,21 +6,12 @@ var Beer     = require('../models/beer.model');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  mongoose.connect(dbConfig.url, function(err) {
-    if (err) {
-      console.error('Could not connect to MongoDB!');
-      console.log(err);
+  return Beer.find({}, function (error, beers) {
+    if (error) {
+      return console.error("Could not query beers.", error);
+    } else {
+      return res.send(beers);
     }
-
-    return Beer.find({}, function (error, beers) {
-      if (error) {
-        return console.error("Could not query beers.", error);
-      } else {
-        return res.send(beers);
-      }
-    })
-  });
 });
 
 module.exports = router;
