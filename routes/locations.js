20029d433d2d9f957463c97fdfa44a89a9e8a9a7
Gregory D. Hopkins <greg.hopkins@gmail.com>
2015-11-22 10:25:11 -0500

    Location/Beer search

--- routes/locations.js
+++ routes/locations.js
@@ -13,15 +13,42 @@ router.get('/', function(req, res, next) {
   var query = {};
 
   if (req.query.beer) {
-    query.beers = { $in: [req.query.beer] }
+    query.beers = {
+      $in: [mongoose.Types.ObjectId(req.query.beer)]
+    }
   }
 
-  Location
-    .find(query)
-    .populate('beers')
-    .then(function(locations) {
-      res.status(200).json(locations);
-    });
+  if (req.query.location) {
+    var lat = Number(req.query.location.split(',')[0]);
+    var lng = Number(req.query.location.split(',')[1]);
+    var point = {
+      type: 'Point',
+      coordinates: [lat, lng]
+    }
+
+    var cmd = {
+      spherical: true,
+      query: query,
+      distanceField: 'distance'
+    };
+
+    if (req.query.radius) {
+      cmd.maxDistance = Number(req.query.radius)
+    }
+
+    Location
+      .geoNear(point, cmd)
+      .then(function(locations) {
+        res.status(200).json(locations);
+      });
+  } else {
+    Location
+      .find(query)
+      .populate('beers')
+      .then(function(locations) {
+        res.status(200).json(locations);
+      });
+  }
 });
 
 module.exports = router;
